a63d7424f1f384f4652880be2abcca8d3d0463fe
Milenka <derumeaux.milenka@gmail.com>
2018-03-22 16:45:29 +0100

    webp in commentaar

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,6 @@ const CriticalPlugin = require('webpack-plugin-critical').CriticalPlugin;
 const merge = require("webpack-merge");
 const parts = require("./webpack.parts");
 
-const port = 3000;
-
 const PATHS = {
   src: path.join(__dirname, "src"),
   dist: path.join(__dirname, "dist")
@@ -42,7 +40,7 @@ const commonConfig = merge([
               loader: `file-loader`,
               options: {
                 limit: 1000,
-                context: `./src/assets/img`,
+                context: `./src`,
                 name: `[path][name].[ext]`
               }
             }, {
@@ -65,9 +63,9 @@ const commonConfig = merge([
                   interlaced: false
                 },
                 // the webp option will enable WEBP
-                webp: {
-                  quality: 75
-                }
+                // webp: {
+                //   quality: 75
+                // }
               }
             }
           ]
