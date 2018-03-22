fe5c9e3f07c217fe700e8b5e0d36f9f0c4f41f4c
Milenka <derumeaux.milenka@gmail.com>
2018-03-22 11:01:11 +0100

    test parallax

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,27 @@
 {
+    const handleParallax = e => {
+        console.log(window.pageYOffset);
+
+        const top = window.pageYOffset;
+
+		const layers = document.getElementsByClassName("parallax");
+		let layer, speed, yPos;
+		for (let i = 0; i < layers.length; i++) {
+			layer = layers[i];
+			speed = layer.getAttribute('data-speed');
+            let yPos = -(top * speed / 10);
+            console.log("yPos: " +yPos)
+			layer.setAttribute('style', 'left: ' + yPos + 'px');
+        }
+        
+        // next step: check height of certain selected element
+        // only start parallax effect at that height 
+    }
+
+    // to do: images come into page on scroll 
 
     const init = () => {
-        console.log("JS file says Hello!");
+        document.addEventListener("scroll", handleParallax);
     }
     
     init();
