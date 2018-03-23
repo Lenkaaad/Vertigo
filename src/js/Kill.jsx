9daec08d47366015bee270668d6bcbda9530b428
Milenka <derumeaux.milenka@gmail.com>
2018-03-23 15:45:07 +0100

    styling + js animation

--- src/js/Kill.jsx
+++ src/js/Kill.jsx
@@ -13,7 +13,7 @@ const Kill = ({data, id, onAdd, onTake}) => {
     
     return (
         <div className="kill">
-            <div class="kill-flex">
+            <div className="kill-flex">
             <img src={data.image} alt=""/>
             <div className="kill-info">
             <h3>{data.name}</h3>
@@ -21,8 +21,8 @@ const Kill = ({data, id, onAdd, onTake}) => {
             </div>
             </div>
             <div className="kill-nav">
-                <button onClick={() => handleTakeCurrent(id)}>Vorige methode</button>
-                <button onClick={() => handleAddCurrent(id)}>Volgende methode</button>
+                <button className="button-prev" onClick={() => handleTakeCurrent(id)}>Vorige methode</button>
+                <button className="button-next" onClick={() => handleAddCurrent(id)}>Volgende methode</button>
             </div>
         </div>
     )
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,68 @@
 {
-    const handleParallax = e => {
-        console.log(window.pageYOffset);
+    const sliderImages = document.querySelectorAll('.slide-in');
+    const contentcontainers = document.querySelectorAll('.content-container');
+
+    const debounce = (func, wait = 20, immediate = true) => {
+        let timeout;
+
+        return function(){
+            const context = this, args = arguments;
+            const later = function() {
+                timeout = null;
+                if(!immediate) func.apply(context, args);
+            };
+
+            const callNow = immediate && !timeout;
+            clearTimeout(timeout);
+
+            timeout = setTimeout(later, wait);
+            if(callNow) func.apply(context, args);
+        }
+    }
+
+    const checkSlide = e => {
+        sliderImages.forEach(sliderImage => {
+            const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+            const imageBottom = sliderImage.offsetTop + sliderImage.height;
+            const isHalfShown = slideInAt > sliderImage.offsetTop;
+            const isNotScrolledPast = window.scrollY < imageBottom;
+
+            if(isHalfShown && isNotScrolledPast) {
+                sliderImage.classList.add('active');
+            }else{
+                sliderImage.classList.remove('active');
+            }
+        });
+    }
+
+    const updateMenu = e => {
+        
+        
+        const currentScroll = window.scrollY + window.innerHeight;
+        // hoever er reeds is gescrolled
+
+        let currentContainer;
+
+        contentcontainers.forEach(container => {
+            const position = container.offsetTop;
+
+            if(position - 1 < currentScroll){
+                currentContainer = container;
+            }
+
+            if(currentContainer){
+            const id = currentContainer.getAttribute('id');
+
+            const currentlyactive = document.querySelector('.active-section');
+            if(currentlyactive){
+                currentlyactive.classList.remove('active-section');
+            }
+            document.querySelector(`[href="#${id}"]`).classList.add('active-section');
+            }
+        })
+    } 
+
+    const handleScroll = e => {
 
         const top = window.pageYOffset;
 
@@ -10,9 +72,11 @@
 			layer = layers[i];
 			speed = layer.getAttribute('data-speed');
             let yPos = -(top * speed / 10);
-            console.log("yPos: " +yPos)
 			layer.setAttribute('style', 'left: ' + yPos + 'px');
         }
+
+        debounce(checkSlide(e));
+        debounce(updateMenu(e), 5);
         
         // next step: check height of certain selected element
         // only start parallax effect at that height 
@@ -21,7 +85,7 @@
     // to do: images come into page on scroll 
 
     const init = () => {
-        document.addEventListener("scroll", handleParallax);
+        document.addEventListener("scroll", handleScroll);
     }
     
     init();
