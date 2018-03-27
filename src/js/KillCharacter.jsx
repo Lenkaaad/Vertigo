34e76cf30807965de3b19935737817109383a1d0
Milenka <derumeaux.milenka@gmail.com>
2018-03-27 13:15:34 +0200

    mobile header + bigger kill images

--- src/js/KillCharacter.jsx
+++ src/js/KillCharacter.jsx
@@ -10,43 +10,43 @@ class KillCharacter extends Component {
         name: "The Gun",
         amount: 33,
         text: `Wist je dat uit alle Hitchcock films, 33 personen werden doodgeschoten?`,
-        image: './assets/img/kill-1-smaller.png'
+        image: './assets/img/kill-1-medium.png'
       },
       2: {
         name: "The Fall",
         amount: 10,
         text: `Wist je dat uit alle Hitchcock films, 10 personen door hoogte stierven?`,
-        image: './assets/img/kill-2-smaller.png'
+        image: './assets/img/kill-2-medium.png'
       },
       3: {
         name: "The Strangled",
         amount: 9,
         text: `Wist je dat uit alle Hitchcock films, 9 personen werden gewurgd?`,
-        image: './assets/img/kill-3-smaller.png'
+        image: './assets/img/kill-3-medium.png'
       },
       4: {
         name: "The Stabbed",
         amount: 8,
         text: `Wist je dat uit alle Hitchcock films, 8 personen werden doorgestoken?`,
-        image: './assets/img/kill-4-smaller.png'
+        image: './assets/img/kill-4-medium.png'
       },
       5: {
         name: "The Suicide",
         amount: 8,
         text: `Wist je dat uit alle Hitchcock films, 8 personen zelfmoord pleegden?`,
-        image: './assets/img/kill-5-smaller.png'
+        image: './assets/img/kill-5-medium.png'
       },
       6: {
         name: "The Drowned",
         amount: 5,
         text: `Wist je dat uit alle Hitchcock films, 5 personen zijn verdronken?`,
-        image: './assets/img/kill-6-smaller.png'
+        image: './assets/img/kill-6-medium.png'
       },
       7: {
         name: "The Birds",
         amount: 2,
         text: `Wist je dat uit alle Hitchcock films, 2 personen stierven door aanvallende vogels?`,
-        image: './assets/img/kill-7-smaller.png'
+        image: './assets/img/kill-7-medium.png'
       }
     },
     'current': {
