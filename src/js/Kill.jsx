7da1aa93396561717376a83f77e7539a70b9a981
Milenka <derumeaux.milenka@gmail.com>
2018-03-28 11:37:50 +0200

    responsive images react-component-1

--- src/js/Kill.jsx
+++ src/js/Kill.jsx
@@ -14,7 +14,32 @@ const Kill = ({data, id, onAdd, onTake}) => {
     return (
         <div className="kill">
             <div className="kill-flex">
-            <img src={data.image} alt=""/>
+            <picture>
+              <source media="(max-width: 134px)" srcSet={data.image + "-100w.webp"} type="image/webp"/>
+              <source media="(max-width: 134px)" srcSet={data.image + "-100w.png"} />
+              <source media="(min-width: 500px)" srcSet={data.image + "-400w.webp"} type="image/webp"/>
+              <source media="(min-width: 500px)" srcSet={data.image + "-400w.png"} />
+              <source media="(min-width: 400px)" srcSet={data.image + "-300w.webp"} type="image/webp"/>
+              <source media="(min-width: 400px)" srcSet={data.image + "-300w.png"} />
+              <source media="(min-width: 135px)" srcSet={data.image + "-200w.webp"} type="image/webp"/>
+              <source media="(min-width: 135px)" srcSet={data.image + "-200w.png"} />
+
+              <img src={data.img + "-400w.png"}
+              alt="Hitchcock's way of killing"
+              className="kill-img"
+              
+              srcSet={data.image + "-400w.png 400w," +
+              data.image + "-300w.png 300w," +
+              data.image + "-200w.png 200w," +
+              data.image + "-100w.png 100w"}
+
+              sizes="50vw"
+              />
+
+
+
+            </picture>
+            {/* <img src={data.image} alt=""/> */}
             <div className="kill-info">
             <h3>{data.name}</h3>
             <p>{data.text}</p>
